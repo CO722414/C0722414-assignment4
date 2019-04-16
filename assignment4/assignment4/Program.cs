87474ee4f436e07eb82e00461f36176ab646a5df
Ramandeep Kaur <722414@student.tor.loc>
2019-04-15 21:06:02 -0400

    section D

--- assignment4/assignment4/Program.cs
+++ assignment4/assignment4/Program.cs
@@ -12,31 +12,45 @@ namespace C0722414_Assignment_4
     class Program
     {
         ArrayList Beowulf;
-
         static void Main(string[] args)
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
+            p.Run();
+            string text = System.IO.File.ReadAllText("U:/Users/722414/c0722414assi4/Beowulf.txt");
+            p.FindNumberOfBlankSpaces(text);
+            p.ProcessArrayList();
+            p.ProcessArrayList1();
+            p.FindNumberOfWords(text);
+        
 
         }
-        public void Run() { this.ReadTextFiles(); }  
+
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
 
         public void ReadTextFiles()
         {
-            //Read file using StreamReader.Reads file line by line 
-            using (StreamReader file = new StreamReader("U:/Users/722414/c0722414assi4Beowulf.txt"))
+            //Read file using StreamReader. Reads file line by line
+            using (StreamReader file = new StreamReader("U:/Users/722414/c0722414assi4/Beowulf.txt"))
             {
                 int counter = 0;
                 string ln;
+
                 while ((ln = file.ReadLine()) != null)
                 {
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
+
+                    counter++;
                 }
+
                 file.Close();
                 Console.WriteLine($"File has {counter} lines.");
-            }
 
+            }
         }
 
         public int FindNumberOfBlankSpaces(string line)
@@ -61,25 +75,47 @@ namespace C0722414_Assignment_4
             Console.WriteLine("Number of letters: " + countletters);
             return countletters;
         }
-
         public int FindNumberOfWords(string x)
         {
             int result = 0;
 
+            //Trim whitespace from beginning and end of string
             x = x.Trim();
 
+            //Necessary because foreach will execute once with empty string returning 1
             if (x == "")
                 return 0;
-            
+
+            //Ensure there is only one space between each word in the passed string
             while (x.Contains("  "))
                 x = x.Replace("  ", " ");
 
+            //Count the words
             foreach (string y in x.Split(' '))
                 result++;
 
             Console.WriteLine("Result is " + result);
             return result;
 
+        }
+
+      
+
+        public void ProcessArrayList()
+        {
+            int LineNumber = 0;
+            foreach (var line in Beowulf)
+
+            {
+
+                if (!ContainWord(line.ToString().ToLower(), "war") && ContainWord(line.ToString().ToLower(), "fare"))
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine("Line number is {0}", LineNumber);
+                    LineNumber++;
+                }
+            }
+            Console.WriteLine(LineNumber);
         }
 
         public void ProcessArrayList1()
@@ -89,7 +125,7 @@ namespace C0722414_Assignment_4
 
             {
 
-                if (ContainWord(line.ToString().ToLower(), "sea") && ContainWord(line.ToString().ToLower(), "fare"))
+                if (!ContainWord(line.ToString().ToLower(), "war") && ContainWord(line.ToString().ToLower(), "fare"))
                 {
                     Console.WriteLine(line);
                     Console.WriteLine("Line number is {0}", LineNumber);
@@ -107,6 +143,6 @@ namespace C0722414_Assignment_4
             }
             return false;
         }
+
     }
 }
-
